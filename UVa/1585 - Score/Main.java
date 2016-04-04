cddb7d3d5936509e39ff4255c5896fc854abbf37
jpvg95 <juan.valencia10a@gmail.com>
2016-04-04 17:11:15 -0500

    Added some problems

--- UVa/1585 - Score/Main.java
+++ UVa/1585 - Score/Main.java
@@ -4,22 +4,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		
-        for(int i=0; i<n; i++){
+		int t = Integer.parseInt(sc.nextLine());
+
+		for (int i=0; i<t; i++) {
 			String l = sc.nextLine();
-			int acumula = 0;
+			int current = 0;
 			int score = 0;
-            int len = l.length();
-			for(int j=0; j<len; j++){
-				if(l.charAt(j)=='O'){					
-					acumula++;
-					score = score+acumula;					
+			int len = l.length();
+			for (int j=0; j<len; j++) {
+				if(l.charAt(j) == 'O'){
+					current++;
+					score = score + current;
 				}else{
-					acumula=0;
+					current = 0;
 				}
-            }
-            System.out.println(score);
+			}
+			System.out.println(score);
 		}
 	}
 }
